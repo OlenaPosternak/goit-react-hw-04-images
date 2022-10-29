883ae0e0df2a56c6ffc349e77fb9062ea6c2e7d4
Olena Posternak <alena.kavyrshyna@gmail.com>
2022-10-29 23:02:02 +0300

    fetch inside app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,21 +5,84 @@ import SearchBar from './SearchBar/SearchBar';
 import ImageGallery from './ImageGallery/ImageGallery';
 
 import { AppStyled } from './App.module';
+import { Loader } from '../components/Loader/Loader';
+import { ButtonMore } from '../components/Button/Button';
 
 import 'react-toastify/dist/ReactToastify.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function App() {
+
   const [searchValue, setSearchValue] = useState('');
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [totalPictures, setTotalPictures] = useState(1);
+
+
+  console.log(searchValue);
+
+  useEffect(() => {
+    // якщо немає слова для пошуку, то запит не робимо
+    if (searchValue === '') {
+      return;
+    }
+
+    setLoading(true);
+
+    async function fetchUrl() {
+      const controller = new AbortController();
+
+      try {
+        const responce = await axios.get('https://pixabay.com/api/', {
+          params: {
+            key: '29526037-011b39b59387f2f37ea2d4748',
+            q: `${searchValue}`,
+            page: `${page}`,
+            image_type: `photo`,
+            orientation: `horizontal`,
+            per_page: 12,
+
+            signal: controller.signal,
+          },
+        });
+
+        setData(prevState => [...prevState, ...responce.data.hits]);
+        setTotalPictures(responce.data.totalHits);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+      return () => {
+        controller.abort();
+      };
+    }
+    fetchUrl(searchValue, page);
+
+  }, [page, searchValue]);
+
+  const loadMore = () => {
+    setPage(prevState=>prevState+1);
+  };
+
 
   const onSubmitSerach = searchInfo => {
     setSearchValue(searchInfo);
+    setData([]);
+    setPage(1);
   };
 
   return (
     <AppStyled>
-      <SearchBar onSearch={onSubmitSerach} />
-      <ImageGallery galleryName={searchValue} />
+      <SearchBar onSearch={onSubmitSerach}  />
+      <ImageGallery galleryName={searchValue} data={data} />
+      {loading && <Loader />}
+
+      {data.length !== 0 && Math.floor(totalPictures / 12) > page && (
+        <ButtonMore onClick={loadMore} />
+      )}
       <ToastContainer autoClose={3000} closeOnClick />
     </AppStyled>
   );
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,68 +1,8 @@
 import PropTypes from 'prop-types';
-
-import { useState, useEffect } from 'react';
 import Item from '../ImageGalleryItem/ImageGalleryItem';
 import { AllGallery } from './ImageGallery.mudule';
-import { Loader } from '../Loader/Loader';
-import { ButtonMore } from '../Button/Button';
-import axios from 'axios';
 
-export default function ImageGallery({ galleryName }) {
-  const [data, setData] = useState([]);
-  const [page, setPage] = useState(1);
-  const [loading, setLoading] = useState(false);
-  const [totalPictures, setTotalPictures] = useState(1);
-
-
-//   якщо змінилось слово дл пошуку, то робимо пустий масив і сторінку скидаємо до 1
-  useEffect(() => {setData([])}, [galleryName]);
-  useEffect(() => {setPage(1)}, [galleryName]);
-
-  useEffect(() => {
-    // якщо немає слова для пошуку, то запит не робимо
-    if (galleryName === '') {
-      return;
-    }
-
-    setLoading(true);
-
-    async function fetchUrl(galleryName, page = 1) {
-      const controller = new AbortController();
-
-      try {
-        const responce = await axios.get('https://pixabay.com/api/', {
-          params: {
-            key: '29526037-011b39b59387f2f37ea2d4748',
-            q: `${galleryName}`,
-            page: `${page}`,
-            image_type: `photo`,
-            orientation: `horizontal`,
-            per_page: 12,
-
-            signal: controller.signal,
-          },
-        });
-
-        setData(prevState => [...prevState, ...responce.data.hits]);
-        setTotalPictures(responce.data.totalHits);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setLoading(false);
-      }
-      return () => {
-        controller.abort();
-      };
-    }
-    fetchUrl(galleryName, page);
-
-    console.log(`1`);
-  }, [page, galleryName]);
-
-  const loadMore = () => {
-    setPage(prevState => prevState + 1);
-  };
-
+export default function ImageGallery({data}) {
   return (
     <>
       <AllGallery>
@@ -75,11 +15,7 @@ export default function ImageGallery({ galleryName }) {
           />
         ))}
       </AllGallery>
-      {loading && <Loader />}
-
-      {data.length !== 0 && Math.floor(totalPictures / 12) > page && (
-        <ButtonMore onClick={loadMore} />
-      )}
+      
     </>
   );
 }
